4075cb24c161e0c9293bbc57d5b3a09f8fd8cf4d
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-02 10:14:04 +0530

    Screens 2

--- app/src/main/java/com/example/pupps/MainActivity.kt
+++ app/src/main/java/com/example/pupps/MainActivity.kt
@@ -1,32 +1,20 @@
 package com.example.pupps
 
+import android.app.Fragment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pupps.Animation.TextAnimationComponent
-import com.example.pupps.Animation.textm
-import com.example.pupps.composable.Analysis
 import com.example.pupps.percieverance.combosables.MyDay
-import com.example.pupps.percieverance.components.Cog
-import com.example.pupps.percieverance.components.HangOverComponent
-import com.example.pupps.percieverance.components.WakeUp
-import com.example.pupps.percieverance.components.scheduledCard
 import com.example.pupps.ui.theme.PuppsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PuppsTheme {
-                MyDay()
-            }
+            fragmentManager.beginTransaction().replace(R.id.main_container,Fragment()).commit()
         }
     }
 }
---
+++ app/src/main/java/com/example/pupps/NewFragment/NewFragment.kt
@@ -0,0 +1,20 @@
+package com.example.pupps.NewFragment
+
+import android.os.Binder
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pupps.R
+
+class NewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.new_fragment,container,false)
+        return view
+    }
+}
